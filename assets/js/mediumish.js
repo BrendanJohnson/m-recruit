1075dd6220ccfbc2f343a6fd5600e3380a27ddfb
Brendan Johnson <brendan@neap.co>
2019-04-09 13:37:32 +1000

    Change folder structure

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 
-    console.log('check cloudcannon cookie')
+    console.log('cloudcannon cookie')
     console.log('cc: ' + document.cookie.indexOf("_cloud_cannon_session"));
     //if cookie hasn't been set display the region select...
     if ((document.cookie.indexOf("RegionSelected")<0) && (document.cookie.indexOf("_cloud_cannon_session")<0)) {
